94a505c15d6e09327712d0cb6367992946a08586
Brandon Man <brandonkingyiuman@gmail.com>
2019-07-17 12:10:49 -0400

    Initial Commit

---
+++ MRG Racecar App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MRG Racecar App
+//
+//  Created by Brandon Man on 17/7/2019.
+//  Copyright © 2019 Brandon Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
